8213eb12b8212681d0b1a3eb08626b2f06689398
aharonoff <tihanyi.aaron@gmail.com>
2025-07-20 02:39:33 +0200

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+let clickCounter = 0;
+
+function buttonClick() {
+    if (clickCounter % 2 == 0) {
+      document.getElementById('myButton').style.backgroundColor = 'yellow';
+    } else {
+      document.getElementById('myButton').style.backgroundColor = 'red';
+    }
+      clickCounter++;
+}
